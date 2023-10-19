9e4f16be07b8e0e39bca7396bcc9ea1b6b806106
jeonbyungho <byho.jeon@gmail.com>
2023-10-19 15:56:04 +0900

    member 엔티티 시퀀스 설정 추가

--- project/src/main/java/com/enums/tourist/domain/Member.java
+++ project/src/main/java/com/enums/tourist/domain/Member.java
@@ -2,16 +2,19 @@ package com.enums.tourist.domain;
 
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity 
 @Getter @Setter
+@SequenceGenerator(name = "member_seq_gen", sequenceName = "member_seq", allocationSize = 1, initialValue = 1)
 public class Member {
 	
-	@Id @GeneratedValue
+	@Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_gen")
 	private Long Id;
 	private String name;
 	private Integer age;
