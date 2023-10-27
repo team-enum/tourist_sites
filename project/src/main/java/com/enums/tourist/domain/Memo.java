a84a695a7cd98ac6bd810b5a44c495d6db22318b
jeonbyungho <byho.jeon@gmail.com>
2023-10-27 12:50:34 +0900

    불 필요한 import 문 삭제

--- project/src/main/java/com/enums/tourist/domain/Memo.java
+++ project/src/main/java/com/enums/tourist/domain/Memo.java
@@ -1,18 +1,13 @@
 package com.enums.tourist.domain;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
--- project/src/main/java/com/enums/tourist/planner/PlannerRepository.java
+++ project/src/main/java/com/enums/tourist/planner/PlannerRepository.java
@@ -1,8 +1,6 @@
 package com.enums.tourist.planner;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.enums.tourist.domain.Planner;
 
