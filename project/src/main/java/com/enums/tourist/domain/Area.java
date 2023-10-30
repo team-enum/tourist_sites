cbfb6e5294882c1a1440a9a5387805aeff98fd01
[jiheon] <wlgjs83@gmail.com>
2023-10-30 15:53:19 +0900

    home.html 검색 목록

---
+++ project/src/main/java/com/enums/tourist/domain/Area.java
@@ -0,0 +1,13 @@
+package com.enums.tourist.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class Area {
+
+   private int areaCode;
+   private String areaName;
+}
---
+++ project/src/main/java/com/enums/tourist/domain/ContentType.java
@@ -0,0 +1,12 @@
+package com.enums.tourist.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class ContentType {
+	 private int contentTypeCode;
+	 private String contentTypeName;
+}
